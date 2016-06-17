b3899e19d7a000b18d6304ace69df5b9b1b96722
Andrew Toast <toastymod@gmail.com>
2016-06-16 18:37:33 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@ echo Toastymod Dashian Chroot
 echo ========================
 cd /mnt/usb/chrootdir
 echo mounting /dev...
-mount -o bind /dev dev/
+mount /dev dev/
 echo done!
 echo mounting /proc...
-mount -o bind /proc proc/
+mount /proc proc/
 echo done!
 echo mounting /sys...
-mount -o bind /sys sys/
+mount /sys sys/
 echo done!
 echo copying resolv.conf...
 cp /etc/resolv.conf etc/resolv.conf
