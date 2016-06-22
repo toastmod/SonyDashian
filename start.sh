9803cd4a5a483ca9ba05fe0a090844eb5a1f7e04
Andrew Toast <toastymod@gmail.com>
2016-06-21 20:27:11 -0600

    Added library copying
    Copies libraries from the Dash fs to the Debian fs.

--- start.sh
+++ start.sh
@@ -1,17 +1,27 @@
-echo ========================
-echo Toastymod Dashian Chroot
-echo ========================
+echo "========================"
+echo "Toastymod Dashian Chroot"
+echo "========================"
 cd /mnt/usb/chrootdir
-echo mounting /dev...
+
+echo "mounting /dev..."
 mount /dev dev/
-echo done!
-echo mounting /proc...
+echo "done!"
+
+echo "mounting /proc..."
 mount /proc proc/
-echo done!
-echo mounting /sys...
+echo "done!"
+
+echo "mounting /sys..."
 mount /sys sys/
-echo done!
-echo copying resolv.conf...
+echo "done!"
+
+echo "copying libraries..."
+cp -a /lib/. lib/
+echo "done!"
+
+echo "copying resolv.conf..."
 cp /etc/resolv.conf etc/resolv.conf
-echo entering debian bash!...
+echo "done!"
+
+echo "entering debian bash!..."
 chroot .
